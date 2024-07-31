5e2b50336b0aa3d547f7fd2396c76e70a4245584
LeyzerGurewitz <leyzer.3770@gmail.com>
2024-07-31 17:09:08 +0300

    LibraryControlr GatAllLibraris()

---
+++ library/Controllers/LibraryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace library.Controllers
+{
+	public class LibraryController : Controller
+	{
+		private readonly ILibraryService _libraryService;
+
+		public LibraryController(ILibraryService libraryService)
+		{
+			_libraryService = libraryService;
+		}
+		public async Task<IActionResult> Index =>
+			await _libraryService.GetAllLibraris.view();
+		
+	}
+}
